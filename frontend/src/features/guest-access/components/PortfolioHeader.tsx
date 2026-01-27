1a3787bbe37bcd594a9f9bff66767c4139f459d1
nithkidd <123559135+nithkidd@users.noreply.github.com>
2026-01-27 23:36:07 +0700

    GA Portfolios Viewpage UI, Fix Landing Page UI

---
+++ frontend/src/features/guest-access/components/PortfolioHeader.tsx
@@ -0,0 +1,10 @@
+export const PortfolioHeader = () => {
+  return (
+    <div className="text-center mb-12">
+      <h2 className="text-5xl md:text-6xl font-black mb-2 text-black">
+        Designers on <span className="text-[#FB8500]">AUREA</span>
+      </h2>
+      <p className="text-gray-600 text-lg">Collection of Designers on AUREA</p>
+    </div>
+  );
+};
